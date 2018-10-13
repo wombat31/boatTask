229b601b0787a4110941bcda6aa8510fee917141
Justin Welham <jwelham@BCAA.AE>
2018-10-13 20:37:43 +0400

    small updates

--- boatTask.js
+++ boatTask.js
@@ -1,8 +1,11 @@
 //create and array
 var boatTimes=["10","10.5","11","11.5","12","12.5","13","13.5","14","14.5","15","15.5","16","16.5","17"];
-var boatOne=["","","","","","","","","","","","","","",""];
+var boatOne=[10,10,10,0,0,0,0,0,0,0,0,0,0,0,0];
 const halfHourCost=12;
 const hourCost=20;
+var addHalfHour = false;
+var timeOut=0;
+var timesAvailable=true;
 
 
 
@@ -15,6 +18,13 @@ function pushData(){
     //get amount of time required
     var timeOut = boatTimes.indexOf(bIn) - boatTimes.indexOf(bOut);
 
+    //check whether half hour is required
+    if (timeOut == 1 || (timeOut%2) != 0){
+        addHalfHour=true;
+    }
+    //check whether boat is available
+    /****CODE  REQUIRED HERE****/
+
     // append data to the array
     //boatOne.push(inputText);
     //var pval="";
@@ -23,7 +33,11 @@ function pushData(){
     //}
 
     //display array data
-    document.getElementById('pText').innerHTML = timeOut;
-    console.log(timeOut,bOut,bIn);
+    //document.getElementById('pText').innerHTML = timeOut;
+    console.log(timeOut,bOut,bIn,addHalfHour,timeOut,timesAvailable);
+    console.log(boatOne);
+    addHalfHour=false;
 }
+
+
 
