322c55a7e01c16e46671c0fc4a7eccbc00a86c31
wombat31 <justin@jbdesign4plans.co.uk>
2018-10-13 11:49:53 -0400

    hire variables added

--- boatTask.js
+++ boatTask.js
@@ -1,6 +1,9 @@
 //create and array
 var boatTimes=["10","10.5","11","11.5","12","12.5","13","13.5","14","14.5","15","15.5","16","16.5","17"];
 var boatOne=["","","","","","","","","","","","","","",""];
+const halfHourCost=12;
+const hourCost=20;
+
 
 
 function pushData(){
@@ -23,3 +26,4 @@ function pushData(){
     document.getElementById('pText').innerHTML = timeOut;
     console.log(timeOut,bOut,bIn);
 }
+
